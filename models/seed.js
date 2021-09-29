0cbce2806c697424bb75474750654364fcffe7ab
Adam Ross Benton <adamrossbenton@gmail.com>
2021-09-29 16:12:11 -0700

    seeded

---
+++ models/seed.js
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////
+// Dependencies
+////////////////////////////////////////////////
+const mongoose = require("./connection")
+const Animal = require("./animal")
+
+////////////////////////////////////////////////
+// Seed
+////////////////////////////////////////////////
+mongoose.connection.on("open", () => {
+    const startAnimals = [
+        {
+            species: "African Lion",
+            extinct: false,
+            location: "Africa",
+            lifeExpectancy: 10,
+        },
+        {
+            species: "River Otter",
+            extinct: false,
+            location: "North America",
+            lifeExpectancy: "8",
+        },
+        {
+            species: "Emperor Penguin",
+            extinct: false,
+            location: "Antarctica",
+            lifeExpectancy: "20",
+        },
+        {
+            species: "Cassowary",
+            extinct: false,
+            location: "Australia",
+            lifeExpectancy: "60",
+        }
+    ];
+    Animal.remove({}, (err,data) => {
+        Animal.create(startAnimals, (err,data) => {
+            console.log("----------ANIMALS CREATED----------")
+            console.log(data)
+            console.log("----------ANIMALS CREATED----------")
+            mongoose.connection.close()
+        })
+    })
+})
